1200ba7cec68b062127c485e90b0046a4a02124d
yud-cumba <yudith.cumba@gmail.com>
2021-06-21 14:31:09 -0500

    refactor spaces

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,18 +1,18 @@
-import React, { Fragment, useState } from 'react'
-import { ListOfProductCards } from '../components/ListOfProductCards'
-import { Helmet } from 'react-helmet'
+import React, { Fragment, useState } from 'react';
+import { ListOfProductCards } from '../components/ListOfProductCards';
+import { Helmet } from 'react-helmet';
 
-import { Search } from '../components/Search'
+import { Search } from '../components/Search';
 export const Home = () => {
-  const [ search , setSearch ] = useState('');
-    return (
-      <Fragment>
-        <Helmet>
-          <title>SoaintShop😄| Pagína principal </title>
-          <meta name='description' content='Visualizar lista de productos' />
+  const [search, setSearch] = useState('');
+  return (
+    <Fragment>
+      <Helmet>
+        <title>SoaintShop😄| Pagína principal </title>
+        <meta name="description" content="Visualizar lista de productos" />
       </Helmet>
-          <Search setSearch={setSearch}/>
-          <ListOfProductCards  search={search}/>
-      </Fragment>
-    )
-  }
+      <Search setSearch={setSearch} />
+      <ListOfProductCards search={search} />
+    </Fragment>
+  );
+};
